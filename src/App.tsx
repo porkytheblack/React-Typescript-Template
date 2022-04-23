923223b9b7c1147a1bd716ddce6ea4698dee6be0
porkytheblack <porkytheblack@gmail.com>
2022-04-23 03:03:48 +0000

    Initial Commit of generic react template

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.less';
+import Router from './Router';
+import {Provider as ReduxProvider} from "react-redux"
+import { store } from './Redux/Store';
+import { ConfigProvider } from 'antd';
+
 
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <ReduxProvider store={store} >
+        <Router/>
+      </ReduxProvider>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,19 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
+import "antd/dist/antd.css"
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {createRoot} from "react-dom/client"
+import React from 'react';
 
-ReactDOM.render(
+const rootElement = document.getElementById("root")
+
+const root  = createRoot(rootElement as HTMLElement)
+
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
